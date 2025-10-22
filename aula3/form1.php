532d81dc5c4e6f35c665e4492a9d10cca629382d
YukidaK <matheus.yuuki.o.k@gmail.com>
2025-10-22 19:24:53 -0300

    teste de commit

---
+++ aula3/form1.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Formulários 1</title>
+</head>
+<body bgcolor="#B0C4DE">
+	<form>
+		Digite seu Nome:
+		<input type="text" name="nome" /><br/>
+
+		Digite sua Idade:		
+		<input type="text" name="idade" />
+	</form>
+</body>
+</html>
